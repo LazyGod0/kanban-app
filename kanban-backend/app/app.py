44d33f2e7b2f657d87c8b1d635fde16999146bcf
LazyGod0 <poomzaza2016@gmail.com>
2026-09-04 08:43:16 +0700

    feat: add CORS for serving frontend

--- kanban-backend/app/app.py
+++ kanban-backend/app/app.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.lib.db import check_database,generate_schema, pool
 from app.routes.auth import router as auth_router
 from app.routes.board import router as board_router
+from app.config.setting import settings
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,5 +17,16 @@ async def lifespan(app: FastAPI):
         await pool.close()
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        settings.frontend_url,
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router)
 app.include_router(board_router)
