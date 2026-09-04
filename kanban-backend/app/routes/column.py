9185b1e26fd89d97cec786f040fe810b3920ddf7
LazyGod0 <poomzaza2016@gmail.com>
2026-09-04 08:08:25 +0700

    refactor: remove unused method and description

--- kanban-backend/app/routes/column.py
+++ kanban-backend/app/routes/column.py
@@ -5,12 +5,14 @@ from app.errors.board import BoardNotFoundException
 from app.lib.db import pool
 from app.models.column import ColumnPayload, ColumnResponse
 from app.repositories.col_repository import ColumnRepository
+from app.routes.task import router as task_router
 from app.services.column.column import ColumnService
 
-router = APIRouter(prefix="/column", tags=["Board Columns"])
+router = APIRouter(prefix="/column")
 column_service = ColumnService(ColumnRepository(pool))
+router.include_router(task_router, prefix="/{column_id}")
 
-@router.post("",response_model=list[ColumnResponse],status_code=status.HTTP_201_CREATED,)
+@router.post("",response_model=list[ColumnResponse],status_code=status.HTTP_201_CREATED,tags=["Board Columns"])
 async def create_columns(board_id: UUID,columns: list[ColumnPayload],user_id: UUID = Depends(get_current_user_id),):
     try:
         return await column_service.create_columns(board_id, user_id, columns)
@@ -25,7 +27,7 @@ async def create_columns(board_id: UUID,columns: list[ColumnPayload],user_id: UU
             detail=str(error),
         ) from error
 
-@router.get("",response_model=list[ColumnResponse])
+@router.get("",response_model=list[ColumnResponse],tags=["Board Columns"])
 async def get_many_columns(
     board_id: UUID,
     user_id: UUID = Depends(get_current_user_id),
@@ -39,7 +41,7 @@ async def get_many_columns(
         ) from error
 
 
-@router.get("/{column_id}",response_model=ColumnResponse)
+@router.get("/{column_id}",response_model=ColumnResponse,tags=["Board Columns"])
 async def get_column(board_id: UUID,column_id: UUID,user_id: UUID = Depends(get_current_user_id),):
     try:
         return await column_service.get_column(board_id, column_id, user_id)
@@ -49,7 +51,7 @@ async def get_column(board_id: UUID,column_id: UUID,user_id: UUID = Depends(get_
             detail=str(error),
         ) from error
 
-@router.patch("/{column_id}",response_model=ColumnResponse)
+@router.patch("/{column_id}",response_model=ColumnResponse,tags=["Board Columns"])
 async def update_column(
     board_id: UUID,
     column_id: UUID,
@@ -72,7 +74,7 @@ async def update_column(
         ) from error
 
 
-@router.delete("/{column_id}",status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{column_id}",status_code=status.HTTP_204_NO_CONTENT,tags=["Board Columns"])
 async def delete_column(
     board_id: UUID,
     column_id: UUID,
